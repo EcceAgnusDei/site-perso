8e9c65dcad011468f1fc81792d5506f324b25ea2
EcceAgnusDei <mondo.antoine@yahoo.fr>
2019-10-16 15:22:47 +0200

    amélioration de la fonctionnalité d'affichage de la page active

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Menu.css';
 
 String.prototype.sansAccent = function(){
@@ -23,14 +23,53 @@ String.prototype.sansAccent = function(){
 
 function Menu(props) {
 	const [activePage, setActivePage] = useState('accueil');
+	const [yPos, setYPos] = useState(0);
+	const yPosRef = useRef(0);
+	const pagesId = [];
+	let pagesY;
+
+	useEffect(() => {
+		pagesY = pagesId.map(id => {
+			return document.getElementById(id).offsetTop + document.getElementById(id).offsetHeight;
+		});
+		window.addEventListener('scroll', () => {
+			if (Math.abs(yPosRef.current - window.scrollY) > 90)
+			{
+				yPosRef.current = window.scrollY;
+				setYPos(window.scrollY);
+
+				if (yPosRef.current < pagesY[0])
+				{
+					setActivePage(pagesId[0]);
+				} 
+				else if (pagesY[0] <= yPosRef.current && yPosRef.current < pagesY[1])
+				{
+					setActivePage(pagesId[1]);
+				}
+				else if (pagesY[1] <= yPosRef.current && yPosRef.current < pagesY[2])
+				{
+					setActivePage(pagesId[2]);
+				}
+				else if (pagesY[2] <= yPosRef.current && yPosRef.current < pagesY[3])
+				{
+					setActivePage(pagesId[3]);
+				}
+				else if (pagesY[3] <= yPosRef.current)
+				{
+					setActivePage(pagesId[4]);
+				}
+			}
+		})
+	},[yPosRef.current])
+
 	const menuJSX = props.children.map(item => {
 		const id = item.props.children.replace(/ /g, "").sansAccent();
+		pagesId.push(id);
 		return (
 		<li key={item.props.children}>
 			<a 
 				className={activePage === id ? "menu-item current ml-4" : "menu-item ml-4"}
 				href={`#${id}`}
-				onClick={() => setActivePage(id)}
 			>
 				{item.props.children}
 			</a>
